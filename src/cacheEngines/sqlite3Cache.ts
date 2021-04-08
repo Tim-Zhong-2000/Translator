951c0c017bcf1ea73d4ad456c9d7f58927bf0b58
Tim-Zhong-2000 <fengyu.fyxdfg@gmail.com>
2021-04-09 01:13:35 +0800

    feat(Cache): sqlite3

--- src/cacheEngines/sqlite3Cache.ts
+++ src/cacheEngines/sqlite3Cache.ts
@@ -2,3 +2,98 @@
  * @description 使用sqlite3的缓存模块
  * @author Tim-Zhong-2000
  */
+
+import sqlite3 = require("sqlite3");
+import { CacheEngine } from "../abstract/cacheEngine";
+import { Payload, SqliteCacheConfig, TranslateLevel } from "../type/type";
+
+export class SqliteCache extends CacheEngine<sqlite3.Database> {
+  exportable = false;
+
+  constructor(config: SqliteCacheConfig) {
+    super();
+    this.db = new sqlite3.Database(config.db.host || ":memory:");
+    this.serivceProviderName = config.serviceProviderName || "unknown";
+    this.exportable = config.exportable || false;
+    this.db.run(
+      "CREATE TABLE IF NOT EXISTS cache (\
+            level       INTERGER  NOT NULL,\
+            src         TEXT      NOT NULL,\
+            dest        TEXT      NOT NULL,\
+            srcLang     TEXT      NOT NULL,\
+            destLang    TEXT      NOT NULL,\
+            tts         BOOLEAN           ,\
+            ttsSrc      TEXT              ,\
+            ttsDest     TEXT               \
+            )"
+    );
+  }
+
+  async fetch(
+    src: string,
+    srcLang: string,
+    destLang: string
+  ): Promise<Payload> {
+    const sqliteProc: Promise<Payload[]> = new Promise((resolve, reject) => {
+      const rows: Payload[] = [];
+      const stmt = this.db.prepare(
+        "SELECT * FROM cache WHERE src=(?) AND srcLang=(?) AND destLang=(?) LIMIT 5"
+      );
+      stmt.run(src, srcLang, destLang);
+      stmt.each((err: Error, row: Payload) => {
+        if (err) reject(err);
+        rows.push(row);
+      });
+      stmt.finalize((err) => {
+        if (err) reject(err);
+        resolve(rows);
+      });
+    });
+    const allResult = await sqliteProc;
+    if (allResult) {
+      console.log(`HIT:\t${decodeURI(src)}`)
+      return this.optimizeResults(allResult);
+    } else {
+      console.log(`MISS:\t${decodeURI(src)}`);
+      throw new Error("MISS");
+    }
+  }
+
+  insert(payload: Payload): void {
+    if (!payload.success) throw new Error("you cant cache a fail payload");
+    const {
+      level,
+      src,
+      dest,
+      srcLang,
+      destLang,
+      tts,
+      ttsSrc,
+      ttsDest,
+    } = payload;
+    const stmt = this.db.prepare("INSERT INTO cache VALUES (?,?,?,?,?,?,?,?)");
+    stmt.run(level, src, dest, srcLang, destLang, tts, ttsSrc, ttsDest);
+    stmt.finalize();
+  }
+
+  optimizeResults(allResults: Payload[]) {
+    const typeArr = allResults.map((result) => result.level);
+    const priority = [
+      TranslateLevel.VERIFIED,
+      TranslateLevel.USER,
+      TranslateLevel.AI,
+    ];
+    for (const type of priority) {
+      const findIndex = typeArr.indexOf(type);
+      if (findIndex >= 0) return allResults[findIndex];
+    }
+    throw new Error("optimizer failed");
+  }
+
+  export() {
+    if (this.exportable) {
+      return "cache type does not support export";
+    }
+    return "cache disabled";
+  }
+}
