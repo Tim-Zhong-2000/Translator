c02ecac1bd813cb2e8e00b79a8aa5abc89e0a9fe
Tim-Zhong-2000 <fengyu.fyxdfg@gmail.com>
2021-04-15 13:02:38 +0800

    feat: add login interface

--- src/user/login.ts
+++ src/user/login.ts
@@ -6,8 +6,18 @@
 import express from "express";
 import session from "express-session";
 import cookieParser from "cookie-parser";
+import { USER } from "../type/type";
 
 const router = express.Router();
 router.use(cookieParser());
-router.get("/login", (req, res) => {});
-router.post("/login", (req, res) => {});
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
+
+router.get("/login", (req, res) => {
+
+});
+
+router.post("/login", (req, res) => {
+    const User:USER.User = req.body;
+    res.setHeader("Set-Cookie",[]);
+});
